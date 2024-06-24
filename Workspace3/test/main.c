1b801d4d41647d5d576910c9274fa60e2a485135
ringo kwon <ringo.kwon@gmail.com>
2024-06-24 23:28:16 +0900

    printf

---
+++ Workspace3/test/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100]; 
+    int i; 
+    
+    printf("Enter ");
+    
+    scanf("%d %s", &i, str);
+    
+    printf("\n you : %d::::%s\n", i , str) ;
+    
+    return 0 ; 
+}
