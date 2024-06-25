e9d17ba7a9dcfbaf4e53b5d6e8c7b8d4b1a4716f
ringo kwon <ringo.kwon@gmail.com>
2024-06-25 14:57:17 +0900

    enum done

---
+++ Workspace3/enum/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    enum Company  { // DEFINING 
+        GOOGLE, 
+        FACEBOOK, 
+        XEROX, YAHOO, EBAY, MICROSOFT
+    };
+    
+    enum Company xerox = XEROX; 
+    enum Company google = GOOGLE; 
+    enum Company ebay = EBAY; 
+    
+     
+    printf("Xerox: %d\n", xerox);
+    printf("Google: %d\n", google);
+    printf("eBay: %d\n", ebay);
+    
+    
+    return 0 ;
+}
+
---
+++ Workspace3/test_double/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+//    printf("dd");
+    double x;
+    scanf("%lf", &x);  
+
+}
