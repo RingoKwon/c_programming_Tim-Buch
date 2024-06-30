0272ff5eccdc69c5d5833b55700a640887ea0584
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-06-30 22:59:54 +0900

    format specifier sol done

--- Workspace3/FormatSpecifier/main.c
+++ Workspace3/FormatSpecifier/main.c
@@ -1,12 +1,23 @@
+/*
+ * author: ringo 
+ * purpose: calculate ractangle 
+
+ * */
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
-    double hight = 5 ; 
-    double width = 2 ; 
-    double perpendicular = (hight + width)* 2 ; 
-    double area = hight * width ; 
+// declaring variables 
+    double hight = 5.0 ; 
+    double width = 2.2 ; 
+    double perimetor; 
+    double area ; 
     
-	printf("hight %.2f\nw: %.2f\np: %.2f\na: %.2f\n", hight, width, perpendicular, area);
+// performing perimeter calculation 
+    perimetor = (hight + width)* 2.0 ;
+    area = hight * width ; 
+// displaying input
+	printf("hight %.2f\nw: %.2f\np: %.2f\na: %.2f\n", hight, width, perimetor, area);
 	return 0;
 }
