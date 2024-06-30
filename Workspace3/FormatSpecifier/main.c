f003fbe58a86ee1319c378d650764bd0f3a678ce
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-06-30 22:45:22 +0900

    format sfecifier my sol

--- Workspace3/FormatSpecifier/main.c
+++ Workspace3/FormatSpecifier/main.c
@@ -2,6 +2,11 @@
 
 int main(int argc, char **argv)
 {
-	printf("Default Project\n");
+    double hight = 5 ; 
+    double width = 2 ; 
+    double perpendicular = (hight + width)* 2 ; 
+    double area = hight * width ; 
+    
+	printf("hight %.2f\nw: %.2f\np: %.2f\na: %.2f\n", hight, width, perpendicular, area);
 	return 0;
 }
