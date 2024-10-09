cc41799aaa89cc9cacc3c2fec89a9afb9582afc9
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-10-09 20:47:36 +0900

    9_val_addign1_perimeter_mysolution

--- c_program_bootcam2024/9_val_assign1_perimeter.c
+++ c_program_bootcam2024/9_val_assign1_perimeter.c
@@ -4,10 +4,20 @@
 int main()
 {
     // declarartion
-    double width;
-    double hight;
-    double perimeter;
+    double _width;
+    double _height;
+    double _perimeter;
     
+    printf("width: ");
+    scanf("%lf", &_width);
 
-    printf("width:");
+    printf("height: ");
+    scanf("%lf", &_height);
+    
+    _perimeter = (_width + _height) * 2;
+
+    printf("perimeter: %.2lf\n", _perimeter);
+
+
+    return 0;
 }
