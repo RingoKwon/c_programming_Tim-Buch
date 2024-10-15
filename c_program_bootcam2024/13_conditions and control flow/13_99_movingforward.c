6fb85377863ec4fdd5c1f8bea141e4c2c492c92e
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-10-15 16:15:05 +0900

    13_99_movefoward mySol

---
+++ c_program_bootcam2024/13_conditions and control flow/13_99_movingforward.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int score;
+
+    printf("Enter you score: \n");
+    scanf("%d", &score);
+
+    if (score >= 80)
+    {
+        printf("Excellent Job! \n");
+    }
+    else if (score >= 60)
+    {
+        printf("Not bad... \n");
+    }
+    else
+    {
+        printf("Fail \n");
+    }
+
+    return 0;
+}
