757cf39ebd8f1df6da7726d813ef31927d1880d3
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-10-15 21:33:53 +0900

    save

--- c_program_bootcam2024/09-10_variable data type/9_val_ex1.c
+++ c_program_bootcam2024/09-10_variable data type/9_val_ex1.c
@@ -8,7 +8,7 @@ int main() {
     scanf("%d", &_age); // 사용자로부터 나이를 입력받아 _age 변수에 저장합니다.
 
     printf("current year:"); // 사용자에게 현재 연도를 입력하라는 메시지를 출력합니다.
-    scanf("%d", &_current); // 사용자로부터 현재 연도를 입력받아 _current 변수에 저장합니다.
+    scanf("%d", &_current);// 사용자로부터 현재 연도를 입력받아 _current 변수에 저장합니다.
 
     // 현재 연도에서 나이를 빼서 출생 연도를 계산하고 출력합니다.
     printf("birth year: %d\n", _current - _age);
