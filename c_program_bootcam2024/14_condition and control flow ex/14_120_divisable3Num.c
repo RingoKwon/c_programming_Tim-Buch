c9d09d4e31a74cbe1ceb3ef29af2673c95bbaf35
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-10-16 21:27:38 +0900

    14_120divisable3Num

---
+++ c_program_bootcam2024/14_condition and control flow ex/14_120_divisable3Num.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int num1, num2, num3; 
+    int set1_2, set1_3, set2_1, set2_3, set3_1, set3_2;
+
+    printf("Enter number thee number: \n");
+    scanf("%d%d%d", &num1, &num2, &num3); 
+
+    set1_2 = num1 % num2;
+    set1_3 = num1 % num3;
+    set2_1 = num2 % num1;
+    set2_3 = num2 % num3;
+    set3_1 = num3 % num1;
+    set3_2 = num3 % num2;
+
+
+    if ( set1_2 == 0 || set1_3 == 0 || set2_1 == 0 || set2_3 == 0 || set3_1 == 0 || set3_2 == 0 ){
+        printf("devisable \n");
+    }else { 
+        printf("Non-devisable \n");
+    }
+
+    return 0; 
+}
