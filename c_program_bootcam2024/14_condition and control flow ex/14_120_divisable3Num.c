fdf67b5f188b2f07c8177ebd722ad7f5afc6adc5
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-10-16 21:40:30 +0900

    14_120_diviable solution

--- c_program_bootcam2024/14_condition and control flow ex/14_120_divisable3Num.c
+++ c_program_bootcam2024/14_condition and control flow ex/14_120_divisable3Num.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main()
+{
 
-    int num1, num2, num3; 
+    int num1, num2, num3;
     int set1_2, set1_3, set2_1, set2_3, set3_1, set3_2;
 
     printf("Enter number thee number: \n");
-    scanf("%d%d%d", &num1, &num2, &num3); 
+    scanf("%d%d%d", &num1, &num2, &num3);
 
     set1_2 = num1 % num2;
     set1_3 = num1 % num3;
@@ -16,12 +17,19 @@ int main() {
     set3_1 = num3 % num1;
     set3_2 = num3 % num2;
 
+    if (num1 == 0 || num2 == 0, num3 == 0)
+    {
+        printf("Cannot devide by Zero");
+    }
 
-    if ( set1_2 == 0 || set1_3 == 0 || set2_1 == 0 || set2_3 == 0 || set3_1 == 0 || set3_2 == 0 ){
+    if ((set1_2 == 0 || set2_1 == 0) && (set1_3 == 0 || set3_1 == 0) && (set2_3 == 0 || set3_2 == 0))
+    {
         printf("devisable \n");
-    }else { 
+    }
+    else
+    {
         printf("Non-devisable \n");
     }
 
-    return 0; 
+    return 0;
 }
