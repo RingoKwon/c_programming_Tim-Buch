eb7b84d9ce8699342b6b63109c8fe1564c9783e3
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-10-18 01:52:22 +0900

    save

---
+++ c_program_bootcam2024/15_HexBineryDecimal/16_152_estrictNumber.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int num;
+
+    printf("Enter Num: \n"); 
+    scanf("%d", &num); 
+    
+    int i = 0; 
+    while (i < num )
+    {
+        printf("*");
+        i++;
+    }
+    
+    return 0; 
+}
