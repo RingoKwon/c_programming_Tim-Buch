2818316a5a39f056058f5fd2794499378c669dc5
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-10-19 05:44:34 +0900

    mySol

---
+++ c_program_bootcam2024/17_Loop_Practice/17_178_mutiplicationTable.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int num, i ; 
+
+    printf("Enter: "); 
+    scanf("%d", &num ); 
+
+    for ( i = 1 ; i <= 10 ; i++){
+        printf("%d x %d = %d \n", num, i, num*i); 
+    }
+
+    return 0; 
+}
