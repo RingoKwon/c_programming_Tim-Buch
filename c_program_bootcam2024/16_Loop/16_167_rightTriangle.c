baba36c51282c4b9a9f5242180c9e3160ece32e1
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-10-19 02:45:05 +0900

    16_167 solution important

--- c_program_bootcam2024/16_Loop/16_167_rightTriangle.c
+++ c_program_bootcam2024/16_Loop/16_167_rightTriangle.c
@@ -8,16 +8,14 @@ int main()
     printf("Enter: ");
     scanf("%d", &n);
 
-    for (i = 1; i < n; i++)
+    for (i = 1; i <= n; i++)
     {
-        printf("%d \n", i);
         for (j = 1; j < i + 1; j++)
         {
-
-            printf("%d", (char)j);
+            printf("%d", j); 
         }
+        printf(" \n");
     }
-    printf("%d \n", n);
 
     return 0;
 }
