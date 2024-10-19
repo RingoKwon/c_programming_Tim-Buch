b9cf592b5906ed0c304f242133735dfa92b7756a
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-10-20 01:16:54 +0900

    mySolution

---
+++ c_program_bootcam2024/19_function practice/19_212_amtAvg.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float avgDigit(int num, int digit)
+{
+    int  digitNum, digitSum=0, j=0 , i=0; 
+    // seperate digit
+    do { 
+        digitNum = num%10;
+        num = num/10; 
+        // average 
+        if (digitNum < digit ){
+            j++; 
+            digitSum += digitNum;
+        }         
+        i++;
+        // printf("%d, %d , j: %d\n", digitNum, digitSum , j); 
+        
+    }while(num > 0 );
+    printf("%d\n", i);
+    return (float)digitSum/j;
+}
+
+int main()
+{
+    int num, digit ;
+    float avg; 
+
+    printf("Enter 'num' and 'digit': ");
+    scanf("%d%d", &num, &digit);
+    printf("\n"); 
+
+    avg = avgDigit(num, digit);
+
+    printf("avg: %.2f \n", avg);
+    return 0;
+}
