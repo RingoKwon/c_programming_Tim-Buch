927cd1881e2d80aeb5d27b10fcef72379eef26fb
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-10-20 01:25:21 +0900

    solution

--- c_program_bootcam2024/19_function practice/19_212_amtAvg.c
+++ c_program_bootcam2024/19_function practice/19_212_amtAvg.c
@@ -5,6 +5,9 @@ float avgDigit(int num, int digit)
 {
     int  digitNum, digitSum=0, j=0 , i=0; 
     // seperate digit
+    if ( num < 0 ){
+        num = num*(-1); 
+    }
     do { 
         digitNum = num%10;
         num = num/10; 
