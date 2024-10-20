8b75d8b95c4e77c193a6e2e630f0e1c6f61fe1d6
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-10-20 13:44:34 +0900

    solution optimization important

--- c_program_bootcam2024/19_function practice/19_215_divisor.c
+++ c_program_bootcam2024/19_function practice/19_215_divisor.c
@@ -19,15 +19,60 @@ int divisor_simple(int num)
         }
     }
     // printf("\n %d", sum);
-    return sum ; 
+    return sum;
+}
+int diviser_simple_2(int num)
+{
+    int i;
+    int sum = 0;
+
+    if (num == 1)
+    {
+        return 1;
+    }
+    for (i = 1; i <= num / 2; i++)
+    {
+        if (num % i == 0)
+        {
+            // printf("%d ", i);
+            sum += i;
+        }
+    }
+    return sum + num;
+}
+int divisoer_optimized(int num)
+{
+    int i;
+    int sum = 0;
+
+    if (num == 1)
+    {
+        return 1;
+    }
+    for (i = 1; i*i <= num ; i++)
+    {
+        if (num % i == 0)
+        {
+            // printf("%d ", i);
+            sum += i;
+            sum += num/i; 
+        }
+    }
+    return sum + num;    
 }
 
 int main()
 {
-    int num = 100;
+    int num = 1000;
 
-    int sum =  divisor_simple(num);
+    int sum = divisor_simple(num);
     printf("%d \n", sum);
 
+    // diviser_simple_2(num);
+    int sum2 = diviser_simple_2(num);
+    printf("%d \n", sum2);
+
+    int sum3 = diviser_simple_2(num);
+    printf("%d \n", sum3);
     return 0;
 }
