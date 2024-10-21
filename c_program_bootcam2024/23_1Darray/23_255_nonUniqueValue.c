989b4d4419a26860922e0cbe0e83b2a10d0c3d8e
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-10-21 15:35:36 +0900

    looping check done

--- c_program_bootcam2024/23_1Darray/23_255_nonUniqueValue.c
+++ c_program_bootcam2024/23_1Darray/23_255_nonUniqueValue.c
@@ -6,43 +6,45 @@
 int main()
 {
 
-    int arr[SIZE] = {3, 1, 3, 2, 2};
+    int arr[SIZE] = {1, 2, 5, 2, 5};
     int i, j;
     int uniqueFlag;
     int cnt = 0;
-
+    //  left side check , right side check
+    //
     for (i = 0; i < SIZE; i++)
     {
         uniqueFlag = 1;
         int cntUnique = 0;
-        for (j = i + 1; j < SIZE; j++)
+
+        for (j = 0; j < i; j++)
         {
+            printf("Li%d, j%d \n", i, j);
             if (arr[i] == arr[j])
             {
-                uniqueFlag = 0;
                 break;
             }
-            // printf("i%d, j%d \n", i,j);
         }
-        if (uniqueFlag == 1)
-        {
-            // printf("%d, \n", arr[i]);
-            cnt++;
-            int k;
-            for (k = 0; k < SIZE; k++)
-            {
-                if (arr[i] == arr[k])
-                {
-                    cntUnique++;
-                }
-            }
-        }
-        if (cntUnique > 1)
-        {
-            printf("%d not unique\n", arr[i]);
-        }
+        for (j = i + 1; j < SIZE; j++)
+            printf("Ri%d, j%d \n", i, j);
+
+        // if (uniqueFlag == 1)
+        // {
+        //     // printf("%d, \n", arr[i]);
+        //     cnt++;
+        //     int k;
+        //     for (k = 0; k < SIZE; k++)
+        //     {
+        //         if (arr[i] == arr[k])
+        //         {
+        //             cntUnique++;
+        //         }
+        //     }
+        // }
+        // if (cntUnique > 1)
+        // {
     }
-    // printf("cnt:%d", cnt); 
+    // printf("cnt:%d", cnt);
 
     return 0;
 }
