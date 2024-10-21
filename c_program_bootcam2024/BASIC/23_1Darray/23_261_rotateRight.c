016352b229651eddbb915272d3a277c07fe5878a
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-10-21 23:25:47 +0900

    mySol update rotation direction

--- c_program_bootcam2024/BASIC/23_1Darray/23_261_rotateRight.c
+++ c_program_bootcam2024/BASIC/23_1Darray/23_261_rotateRight.c
@@ -7,17 +7,17 @@ int main()
 
     int arr[SIZE] = {1, 2, 3, 4, 5};
     int i, j;
-    int n = 2;
+    int n = 1;
 
     for (j = 0; j < n; j++)
     {
 
-        int temp = arr[0];
-        for (i = 0; i < SIZE - 1; i++)
+        int temp = arr[SIZE-1];
+        for (i = SIZE-1; i > 0 ; i--)
         {
-            arr[i] = arr[i+1] ;
+            arr[i] = arr[i-1] ;
         }
-        arr[SIZE-1] = temp;
+        arr[0] = temp;
     }
 
     for (i = 0; i < SIZE; i++)
