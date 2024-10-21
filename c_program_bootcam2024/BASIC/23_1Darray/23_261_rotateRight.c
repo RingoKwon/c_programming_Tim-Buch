ae27f0426bb6b4939c30cc46b576558dd4784235
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-10-21 23:19:00 +0900

    mySol

---
+++ c_program_bootcam2024/BASIC/23_1Darray/23_261_rotateRight.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 5
+
+int main()
+{
+
+    int arr[SIZE] = {1, 2, 3, 4, 5};
+    int i, j;
+    int n = 2;
+
+    for (j = 0; j < n; j++)
+    {
+
+        int temp = arr[0];
+        for (i = 0; i < SIZE - 1; i++)
+        {
+            arr[i] = arr[i+1] ;
+        }
+        arr[SIZE-1] = temp;
+    }
+
+    for (i = 0; i < SIZE; i++)
+    {
+        printf("%d", arr[i]);
+    }
+
+    return 0;
+}
