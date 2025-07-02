7a43ad2dc1e57c1581dbef77c5e7847f3c4ef829
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-07-02 16:42:08 +0900

    solution

--- c_program_bootcam2024/BASIC/24_string/24_273_length_solution.c
+++ c_program_bootcam2024/BASIC/24_string/24_273_length_solution.c
@@ -7,6 +7,11 @@ int leng(char *str)
     int i = 0;
     while (str[i] != '\0')
     {
+        if (str[i] == '\n')
+        {
+            str[i] = '\0';
+            break;
+        }
         i++;
     }
 
@@ -18,8 +23,8 @@ int main()
     int stringLength;
     char input[SIZE];
     fgets(input, SIZE, stdin);
-    stringLength = leng(input)-1;
-    printf("%d", stringLength);
+    stringLength = leng(input) ;
+    printf("The length of string '%s' is %d", input, stringLength);
 
     return 0;
 }
