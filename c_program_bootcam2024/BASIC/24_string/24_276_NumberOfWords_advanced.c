124eb2d7e875738237e84c00874dc1b09977b6a0
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-07-02 19:25:41 +0900

    my solution

---
+++ c_program_bootcam2024/BASIC/24_string/24_276_NumberOfWords_advanced.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char dd[100] = "  a b   c   ";
+    int i;
+    int cnt =0; 
+    // count space right after the character
+    for (i = 0; i < strlen(dd); i++)
+    {
+        if (i != 0)
+        {
+            if (dd[i - 1] != ' ' && dd[i] == ' ')
+            {
+                cnt++; 
+
+            }
+        }
+    }
+    printf("%d", cnt);
+    return 0;
+}
