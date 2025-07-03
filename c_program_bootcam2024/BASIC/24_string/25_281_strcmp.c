992b9f04322a0a204bfad2f3868775acf426dff1
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-07-03 20:03:22 +0900

    code review

--- c_program_bootcam2024/BASIC/24_string/25_281_strcmp.c
+++ c_program_bootcam2024/BASIC/24_string/25_281_strcmp.c
@@ -25,93 +25,41 @@ char *lower(char *input)
     output[i] = '\0';
     return output;
 }
-int strLen(char *str)
-{
-    int i = 0;
-    while (str[i] != '\0')
-    {
-        i++;
-    }
-    return i;
-}
+// int strLen(char *str)
+// {
+//     int i = 0;
+//     while (str[i] != '\0')
+//     {
+//         i++;
+//     }
+//     return i;
+// }
 int strcmp(char *cmp1, char *cmp2)
 {
-    int len1 = strLen(cmp1);
-    int len2 = strLen(cmp2);
-    int output;
-    int minLen;
-
-    if (len1 > len2)
-    {
-        minLen = len2;
-    }
-    else
-    {
-        minLen = len1;
-    }
-
     int i = 0;
-    if (len1 == len2)
+    while (cmp1[i] != '\0' && cmp2[i] != '\0')
     {
-        for (i = 0; i < len1; i++)
-        {
-            if (cmp1[i] < cmp2[i])
-            {
-                output = -1;
-                break;
-            }
-            else if (cmp1[i] > cmp2[i])
-            {
-                output = 1;
-                break;
-            }
-            else
-            {
-                output = 0;
-            }
-        }
+        if (cmp1[i] < cmp2[i])
+            return -1;
+        else if (cmp1[i] > cmp2[i])
+            return 1;
+        i++;
     }
-    else
-    {
-        for (i = 0; i < minLen; i++)
-        {
-            if (cmp1[i] < cmp2[i])
-            {
-                output = -1;
-                break;
-            }
-            else if (cmp1[i] > cmp2[i])
-            {
-                output = 1;
-                break;
-            }
-            else
-            {
-                if (len1 > len2)
-                {
-                    output = 1;
-                }
-                else
-                {
-                    output = -1;
-                }
-            }
-        }
-    }
-    // 길이 같고 모두 같으면 같다
-    // 길이 다르고 짧은거 기준 같은경우 짧은게 우선
-    // 나머지는 짧은거 기준 최초 다른거 기준
-
-    return output;
+    if (cmp1[i] == '\0' && cmp2[i] == '\0')
+        return 0; 
+    else if ( cmp1[i] == '\0')
+        return -1; 
+    else 
+        return 1; 
 }
 
 int main()
 {
     int c;
 
-       c = strcmp("bcc", "bcca");
+    c = strcmp("bcc", "bcca");
     printf("%d\n", c);
 
     return 0;
 }
-//  코드리뷰 필요함 !! 
+//  코드리뷰 필요함 !!
