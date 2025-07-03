d069e9e8bc51216d588321138d6dc4905452c641
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-07-03 15:07:28 +0900

    function  not working

---
+++ c_program_bootcam2024/BASIC/24_string/25_281_strcmp.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+// #include <string.h>
+
+void lower(char input ) {
+    if(input >= 65 && input <= 90)
+    input = input +32; 
+    // upper to lower 
+    return input;
+}
+int strcmp(char *cmp1, char *cmp2)
+{
+
+    return 0;
+}
+
+int main()
+{
+    bool a;
+    char b;
+    a = 'D' == 69;
+    printf("%d", a);
+    b = lower('S');
+    printf("%c",b);
+    
+    return 0;
+}
