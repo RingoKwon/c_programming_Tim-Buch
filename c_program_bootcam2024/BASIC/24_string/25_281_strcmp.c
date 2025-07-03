bb043f694f598fd744af3860aacf1fda9312a5fa
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-07-03 19:11:53 +0900

    leng check completed

--- c_program_bootcam2024/BASIC/24_string/25_281_strcmp.c
+++ c_program_bootcam2024/BASIC/24_string/25_281_strcmp.c
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 // #include <string.h>
 
-char* lower(char *input)
+char *lower(char *input)
 {
     int i = 0;
     static char output[300];
@@ -11,7 +11,7 @@ char* lower(char *input)
     while (input[i] != '\0')
     {
         /* code */
-        if (input[i] >= 65 && input[i] <= 90)
+        if (input[i] >= 65 && input[i] <= 90) // 문자 'A', 'Z' 사용 가능
         {
             output[i] = input[i] + 32;
         }
@@ -25,20 +25,55 @@ char* lower(char *input)
     output[i] = '\0';
     return output;
 }
+int strLen(char *str)
+{
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        i++;
+    }
+    return i;
+}
 int strcmp(char *cmp1, char *cmp2)
 {
+    int len1 = strLen(cmp1);
+    int len2 = strLen(cmp2);
+    int lenLong;
+    if (len1 >= len2)
+    {
+        lenLong = len1;
+    }
+    else
+    {
+        lenLong = len2;
+    }
+// 길이 같고 모두 같으면 같다 
+// 길이 다르고 짧은거 기준 같은경우 짧은게 우선 
+// 나머지는 짧은거 기준 최초 다른거 기준 
+    int i = 0;
+    for (i; lenLong, i++)
+    {
+        
+    }
 
-    return 0;
+    return lenLong;
 }
 
 int main()
 {
     bool a;
     char *b;
+    int c;
+
     a = 'D' == 69;
-    printf("%d", a);
+    printf("%d\n", a);
     b = lower("Sdd");
-    printf("%s", b);
+    printf("%s\n", b);
+    c = strLen("dd");
+    printf("%d\n", c);
+
+    c = strcmp("d", "d");
+    printf("%d\n", c);
 
     return 0;
 }
