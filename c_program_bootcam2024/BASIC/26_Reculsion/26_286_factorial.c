74e3bd427596a948bc90b907736fc9969be917f7
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-07-04 14:14:19 +0900

    Fix factorial function to use recursion correctly
    Corrected the factorial function to call itself recursively instead of performing a simple multiplication. Updated main to print the correct result of the factorial calculation.

--- c_program_bootcam2024/BASIC/26_Reculsion/26_286_factorial.c
+++ c_program_bootcam2024/BASIC/26_Reculsion/26_286_factorial.c
@@ -6,14 +6,16 @@ int facto(int input)
 {
     if (input == 1)
         return 1; 
-    return input * ( input -1 );
+    return input * facto( input -1 );
 }
 int main()
 {
     int num;
+    int result; 
+    
     printf("Enter Num: ");
     scanf("%d", &num);
-    facto(num);
-    printf("Answer: %d", num);
+    result= facto(num);
+    printf("Answer: %d", result);
     return 0;
 }
