e045934365d017899a1de65204e063910f24875b
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-07-04 16:47:47 +0900

    ai help

--- c_program_bootcam2024/BASIC/26_Reculsion/26_290_sumOfDigits.c
+++ c_program_bootcam2024/BASIC/26_Reculsion/26_290_sumOfDigits.c
@@ -3,18 +3,22 @@
 #include <string.h>
 #include <math.h>
 
-int sumDigit(int num ,int  i )
+int sumDigit(int num, int i)
 {
 
     int denom = pow(10, i);
-    if (denom >= num) 
-        return  num / denom; 
-    int remainder = num % denom;
-    printf("remainder: %d, i: %d, num : %d add: %d denom: %d\n", remainder, i, num, remainder/(denom/10), denom);
-
-    num = num - remainder;
-
-    return remainder/denom + sumDigit(num, i+1 );
+    if (denom >= num)
+        return num / denom;
+    
+    // 현재 자릿수 추출
+    int digit = (num % denom) / (denom / 10);
+    
+    printf("자릿수 %d 추출, i: %d\n", digit, i);
+    
+    // 남은 숫자에서 현재 자릿수 제거
+    int remaining = num - (num % denom);
+    
+    return digit + sumDigit(remaining + (num % (denom/10)), i + 1);
 }
 
 int main()
@@ -22,6 +26,6 @@ int main()
     int num;
     printf("Enter num: ");
     scanf("%d", &num);
-    printf("%d", sumDigit(num, 1 ));
+    printf("%d", sumDigit(num, 1));
     return 0;
 }
