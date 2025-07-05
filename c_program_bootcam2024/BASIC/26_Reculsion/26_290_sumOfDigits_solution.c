6614dd2af4a09185ab27dc78818f7b979415f35c
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-07-05 09:40:10 +0900

    Update .gitignore and add recursion C examples
    Refined .gitignore patterns to better exclude compiled executables and non-source files in the BASIC directory structure. Added new C files for recursion practice, including a sum of digits solution, a template file, and a test file.

---
+++ c_program_bootcam2024/BASIC/26_Reculsion/26_290_sumOfDigits_solution.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int sumDigit(int num)
+{
+    if ( num < 9 )
+        return num; 
+    return num%10 +sumDigit(num/10);
+}
+
+
+int main()
+{   
+    int input ; 
+    printf("Enter: ") ;
+    scanf("%d", &input); 
+    
+    printf("Answer: %d", sumDigit(input));
+	return 0;
+}
