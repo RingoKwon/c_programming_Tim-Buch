1b495aa3839dca6983d67301de6a8461637b3f7b
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-07-05 10:42:50 +0900

    Add initial recursive number check function
    Created 27_290_totalNumLessNum.c with a stub for a recursive function to check numbers less than a given value. Added basic main function structure for user input.

---
+++ c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_290_totalNumLessNum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// int list 입력 기능 
+// recursive로 num 보다 작은 숫자 판단기준 
+// 입력도 recursive로? -> no need list 
+int numCheckUnder(int num)
+{
+    printf("%d");
+}
+int main()
+{
+    printf("Enter: ");
+    scanf("%d", &num);
+	return 0;
+}
