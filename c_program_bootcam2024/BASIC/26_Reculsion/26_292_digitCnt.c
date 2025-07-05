99957d6ac69c568b2a9375b0456fbc63d34b47b9
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-07-05 09:51:23 +0900

    my solution

---
+++ c_program_bootcam2024/BASIC/26_Reculsion/26_292_digitCnt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int digitCnt(int num)
+{
+    if (num < 9)
+        return 1; 
+    return 1 + digitCnt(num/10);
+}
+int main()
+{
+	int input; 
+    printf("Enter: "); 
+    scanf("%d", &input); 
+    printf("Answer: %d", digitCnt(input)); 
+    return 0;
+}
