ac1a035dd8109339410cba5307b9aef16b878fe9
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-07-07 10:54:17 +0900

    Refactor recursion to input and count numbers less than 10
    Modified numCheckUnder to recursively input numbers and count how many are less than 10 until -1 is entered. Updated main to prompt for a threshold (currently unused) and call the new recursive function.

--- c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_290_totalNumLessNum.c
+++ c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_290_totalNumLessNum.c
@@ -2,16 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-// int list 입력 기능 
-// recursive로 num 보다 작은 숫자 판단기준 
-// 입력도 recursive로? -> no need list 
-int numCheckUnder(int num)
+// int list 입력 기능
+// recursive로 num 보다 작은 숫자 판단기준
+// 입력도 recursive로? -> no need list
+int numCheckUnder()
 {
-    printf("%d");
+
+    int temp = 10;
+    int input;
+    int output;
+
+    printf("Enter number: ");
+    scanf("%d", &input);
+
+    if (input == -1)
+        return output ;
+    if (input < temp)
+    {
+        output = 1;
+    }
+    else
+    {
+        output = 0;
+    }
+
+    
+    return output+numCheckUnder();
 }
 int main()
 {
-    printf("Enter: ");
+    int num;
+
+    printf("Enter threshold: ");
     scanf("%d", &num);
-	return 0;
+    
+    return numCheckUnder();;
 }
