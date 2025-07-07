169680973f5a609658e9d6f75ece823fd3012324
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-07-07 13:15:27 +0900

    my solution

--- c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_290_totalNumLessNum.c
+++ c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_290_totalNumLessNum.c
@@ -5,36 +5,36 @@
 // int list 입력 기능
 // recursive로 num 보다 작은 숫자 판단기준
 // 입력도 recursive로? -> no need list
-int numCheckUnder()
+int numCheckUnder(int previous, int threshold)
 {
 
-    int temp = 10;
+
     int input;
-    int output;
+    int output = previous;
 
     printf("Enter number: ");
     scanf("%d", &input);
 
     if (input == -1)
-        return output ;
-    if (input < temp)
+        return output;
+    if (input < threshold)
     {
-        output = 1;
+        output += 1;
     }
     else
     {
-        output = 0;
+        output += 0;
     }
-
-    
-    return output+numCheckUnder();
+    return  numCheckUnder(output, threshold);
 }
 int main()
 {
     int num;
+    int result;
 
     printf("Enter threshold: ");
     scanf("%d", &num);
-    
-    return numCheckUnder();;
+    result = numCheckUnder(0, num);
+    printf("Result: %d", result);
+    return 0;
 }
