f93adb388cc505dd0033790c4307b6147ebd6c37
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-07-09 11:14:35 +0900

    solution

--- c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_290_totalNumLessNum_solution.c
+++ c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_290_totalNumLessNum_solution.c
@@ -4,10 +4,21 @@
 
 int numLessCnt(int num)
 {
-    int input ; 
-    printf("enter: ")
+    int input;
+    printf("enter: ");
+    scanf("%d", &input);
+    if (input == -1)
+        return 0;
+    if (input < num)
+        return 1 + numLessCnt(num);
+    return numLessCnt(num);
 }
 int main()
 {
+    int thresh;
+    printf("Threshold: ");
+    scanf("%d", &thresh);
+    printf("%d" , numLessCnt(thresh));
+
     return 0;
 }
