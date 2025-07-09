ed120fb32e42f834c72503bd42ade8f592024056
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-07-09 17:44:31 +0900

    ai solution

--- c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_295_charSeq.c
+++ c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_295_charSeq.c
@@ -6,22 +6,21 @@ int checkNum(char threshold)
 {
     char input;
     printf("char_check: ");
-    scanf("%c", &input);
+    scanf( " %c", &input);
 
     if (input == '$')
         return 0;
     if (input == threshold)
-        return 1 + checkNum(input);
-    return checkNum(input);
+        return 1 + checkNum(threshold);  // threshold 유지
+    return checkNum(threshold);          // threshold 유지
 }
 
 int main()
 {
     char _char;
     printf("char: ");
-    scanf("%c", &_char);
+    scanf(" %c", &_char);
     
     printf("%d", checkNum(_char));
     return 0;
-    ;
 }
