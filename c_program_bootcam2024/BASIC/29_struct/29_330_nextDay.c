7a4887c6c149cc219cc4410c2206e44ec74566de
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-07-10 10:12:54 +0900

    Add printDay and setDate functions for Date struct
    Introduced printDay to display a Date struct and setDate to input a Date from the user. Updated main to use printDay for output.

--- c_program_bootcam2024/BASIC/29_struct/29_330_nextDay.c
+++ c_program_bootcam2024/BASIC/29_struct/29_330_nextDay.c
@@ -9,11 +9,31 @@ typedef struct date
     int year;
 } Date;
 
+void printDay(Date dt)
+{
+    printf("date\nday: %d, month: %d, year: %d\n", dt.day, dt.month, dt.year);
+}
+
+Date setDate()
+{
+    Date dt; 
+    printf("d:");
+    scanf("%d", &dt.day);
+    printf("m:");
+    scanf("%d", &dt.month);
+    printf("y:");
+    scanf("%d", &dt.year);
+
+    return dt;
+}
+
 int main()
 {
-    Date day; 
-    day.day = 1 ;
-    day.month = 3; 
+    Date day;
+    day.day = 1;
+    day.month = 3;
     day.year = 2021;
+
+    printDay(day);
     return 0;
 }
