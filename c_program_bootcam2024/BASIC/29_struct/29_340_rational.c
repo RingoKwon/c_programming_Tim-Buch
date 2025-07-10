74c7a8355e81e0bf87673c128cd1ce89abe465e5
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-07-10 20:49:05 +0900

    Add multiplication and division for Rational struct
    Implemented mutiple and division functions for Rational numbers and updated main to demonstrate their usage. This enhances the Rational struct operations to include multiplication and division alongside addition and subtraction.

--- c_program_bootcam2024/BASIC/29_struct/29_340_rational.c
+++ c_program_bootcam2024/BASIC/29_struct/29_340_rational.c
@@ -43,46 +43,56 @@ Rational sub(Rational input1, Rational input2)
     return output;
 }
 
+Rational mutiple(Rational input1, Rational input2)
+{
+    Rational output;
+    output.numerator = input1.numerator * input2.numerator;
+    output.denominator = input1.denominator * input2.denominator;
+    return output; 
+}
+Rational division(Rational input1, Rational input2)
+{
+    Rational output;
+    output.numerator = input1.numerator * input2.denominator;
+    output.denominator = input1.denominator * input2.numerator;
+    return output; 
+}
+
 //  print
 void print_single(Rational input)
 {
     double float_num = 1.00 * input.numerator / input.denominator;
     printf("Output: %.2f(%d/%d)\n", float_num, input.numerator, input.denominator);
 }
-// print double factor 
-
+// print double factor
 
 int main()
 {
     Rational a1;
     a1.denominator = 2;
     a1.numerator = 3;
-    Rational a2; 
-    a2.denominator = 4; 
-    a2.numerator = 7; 
-    
+    Rational a2;
+    a2.denominator = 4;
+    a2.numerator = 7;
 
     printf("in1: %d/%d, in2: %d/%d\n", a1.numerator, a1.denominator, a2.numerator, a2.denominator);
-    
+
     Rational output_increment = increment(a1);
     Rational output_decrement = decrement(a1);
     printf("icrement, decrement\n");
     print_single(output_increment);
     print_single(output_decrement);
-    
+
     printf("addition, subscription\n");
-    Rational out_add = add(a1, a2); 
+    Rational out_add = add(a1, a2);
     print_single(out_add);
-    Rational out_sub = sub(a1, a2); 
+    Rational out_sub = sub(a1, a2);
     print_single(out_sub);
-
-
-
-
-
-
-
-
-
+    
+    printf("mutiplication, division\n");
+    Rational out_mul = mutiple(a1, a2);
+    print_single(out_mul);
+    Rational out_div = division(a1, a2);
+    print_single(out_div);
     return 0;
 }
