4f6e385a4ef014de34788b2f4329722911b0608c
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-07-10 20:15:41 +0900

    Add add and print_double functions for Rational struct
    Introduced a two-operand add function to sum two Rational numbers and a print_double function to display two Rational values. Also improved formatting and added comments for clarity.

--- c_program_bootcam2024/BASIC/29_struct/29_340_rational.c
+++ c_program_bootcam2024/BASIC/29_struct/29_340_rational.c
@@ -13,6 +13,8 @@ typedef struct rational
     int denominator;
 } Rational;
 
+// function
+//  single factor fuction
 Rational increment(Rational input)
 {
     input.numerator += input.denominator;
@@ -23,10 +25,26 @@ Rational decrement(Rational input)
     input.numerator -= input.denominator;
     return input;
 }
+// tow factor function
+Rational add(Rational input1, Rational input2)
+{
+    Rational output;
+    output.denominator = input1.denominator * input2.denominator;
+    output.numerator = (input1.numerator * input2.denominator) +
+                       (input1.denominator * input2.numerator);
+    return output;
+}
+
+//  print
 void print_single(Rational input)
 {
-    double float_num =1.00* input.numerator/input.denominator;
+    double float_num = 1.00 * input.numerator / input.denominator;
     printf("Output: %.2f(%d/%d)\n", float_num, input.numerator, input.denominator);
+}
+// print double factor 
+void print_double(Rational in1, Rational in2 )
+{
+    printf("in2 %d/%d, in2 %d/%d\n", in1.numerator, in1.denominator, in2.numerator, in2.denominator);
 }
 
 int main()
@@ -34,11 +52,20 @@ int main()
     Rational a1;
     a1.denominator = 2;
     a1.numerator = 3;
+    Rational a2; 
+    a2.denominator = 4; 
+    a2.numerator = 7; 
+    
 
-    Rational output_increment = increment(a1); 
-    Rational output_decrement = decrement(a1); 
+    
+    Rational output_increment = increment(a1);
+    Rational output_decrement = decrement(a1);
     print_single(output_increment);
     print_single(output_decrement);
+
+    Rational
+
+
 
     return 0;
 }
