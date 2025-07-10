bdc0c18f3d3243e5f1fff0d8ae67cf41daac9b22
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-07-10 09:45:58 +0900

    Add setPoint function and improve printPoint output
    Introduced a setPoint function to input coordinate values from the user and updated printPoint to format its output more clearly. main now demonstrates input and output for two Coordinate instances.

--- c_program_bootcam2024/BASIC/29_struct/29_329_typeof.c
+++ c_program_bootcam2024/BASIC/29_struct/29_329_typeof.c
@@ -10,12 +10,27 @@ typedef struct coordinate
 
 void printPoint(Coordinate input)
 {
-    printf("x: %d\n y: %d", input.x, input.y);
+    printf("---output---");
+    printf("\nx: %d\ny: %d\n", input.x, input.y);
 }
-
+Coordinate setPoint()
+{
+    int x, y;
+    Coordinate point;
+    printf("---input---\n");
+    printf("x: ");
+    scanf("%d", &point.x);
+    printf("y: ");
+    scanf("%d", &point.y);
+    return point;
+}
 
 int main()
 {
-
+    Coordinate point1, point2;
+    point1 = setPoint();
+    printPoint(point1);
+    point2 = setPoint();
+    printPoint(point2);
     return 0;
 }
