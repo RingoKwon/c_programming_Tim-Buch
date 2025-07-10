5954aa0e392e7765f55e95b69d527bf91a0f9934
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-07-10 19:43:59 +0900

    Add increment, decrement, and print functions for Rational
    Introduced increment and decrement functions to modify Rational numbers, and a print_single function to display Rational values in both float and fraction formats. Updated main to demonstrate these new functions.

--- c_program_bootcam2024/BASIC/29_struct/29_340_rational.c
+++ c_program_bootcam2024/BASIC/29_struct/29_340_rational.c
@@ -4,6 +4,8 @@
 /*
 - struct: num, denum
 - need to make same denum for addition , substraction,
+- how you gonna print it? input can be different on fuctions. -> print fuction should be differ
+-
 */
 typedef struct rational
 {
@@ -11,11 +13,32 @@ typedef struct rational
     int denominator;
 } Rational;
 
+Rational increment(Rational input)
+{
+    input.numerator += input.denominator;
+    return input;
+}
+Rational decrement(Rational input)
+{
+    input.numerator -= input.denominator;
+    return input;
+}
+void print_single(Rational input)
+{
+    double float_num =1.00* input.numerator/input.denominator;
+    printf("Output: %.2f(%d/%d)", float_num, input.numerator, input.denominator);
+}
+
 int main()
 {
     Rational a1;
     a1.denominator = 2;
     a1.numerator = 3;
-    printf("%f", 1.0* a1.numerator/a1.denominator);
+
+    Rational output_increment = increment(a1); 
+    Rational output_decrement = decrement(a1); 
+    print_single(output_increment);
+    print_single(output_decrement);
+    printf("%f", 1.0 * a1.numerator / a1.denominator);
     return 0;
 }
