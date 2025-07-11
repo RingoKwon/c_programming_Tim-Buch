f082ae27c9e69a1194b51cf6beacce37c37bae56
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-07-11 14:38:19 +0900

    Fix and display element count in array
    Initialized elementCnt array to size 6 and added a loop to print the count of each element. This ensures correct counting and output for all possible values in sequenceArray.

--- c_program_bootcam2024/BASIC/32_cntArray.c/32_354_ex1.c
+++ c_program_bootcam2024/BASIC/32_cntArray.c/32_354_ex1.c
@@ -6,12 +6,18 @@
 int main()
 {
     int sequenceArray[SIZE] = {0, 5, 4, 2, 1, 3, 2, 0};
-    int elementCnt[5] ; 
+    int elementCnt[6] ={0} ;/// array initalization needed!!  6 not 5 
     int i ; 
 
     for ( i = 0; i < SIZE ; i++)
     {
         elementCnt[sequenceArray[i]]++;
+
+    } 
+    int j; 
+    for (j = 0 ; j < 6; j++)
+    {
+        printf("idx: %d, cnt: %d\n", j, elementCnt[j]);
     }
     return 0;
 }
