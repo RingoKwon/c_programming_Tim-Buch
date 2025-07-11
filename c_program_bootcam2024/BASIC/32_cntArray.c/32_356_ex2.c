15c7c994c65f01db43452c0170c05ca24a514226
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-07-11 15:38:38 +0900

    Add most frequent number output to array counter
    Added logic to determine and print the most frequent number in the source array, along with its count. Improved formatting and removed unused elements from the source array initialization.

--- c_program_bootcam2024/BASIC/32_cntArray.c/32_356_ex2.c
+++ c_program_bootcam2024/BASIC/32_cntArray.c/32_356_ex2.c
@@ -5,22 +5,35 @@
 
 int main()
 {
-    int sourceArr[SIZE] = {0,3,4,5,6,7,8,6,5,4,4,4,4,4,3,3,0,1,1,1}; 
-    int countArr[10] = {0}; 
+    int sourceArr[SIZE] = {0, 3, 4, 5, 6, 7, 8, 6, 5, 4, 4, 4, 4, 4, 3, 3, 0, 1};
+    int countArr[10] = {0};
 
-    int i ; 
-    for ( i = 0 ; i < SIZE; i++)
+    int i;
+    for (i = 0; i < SIZE; i++)
     {
         countArr[sourceArr[i]]++;
     }
-    
+
     // 결과 출력
     printf("Number frequency:\n");
-    for (i = 0; i < 10; i++) {
-        if (countArr[i] > 0) {
+    for (i = 0; i < 10; i++)
+    {
+        if (countArr[i] > 0)
+        {
             printf("Number %d appears %d times\n", i, countArr[i]);
         }
     }
 
-	return 0;
+    int idx = 0;
+    for (i = 0; i < 9; i++)
+    {
+        if (countArr[i] >= countArr[idx])
+        {
+
+            idx = i;
+        }
+    }
+    printf("Most frequant number: %d(%d times)", idx, countArr[idx]);
+
+    return 0;
 }
