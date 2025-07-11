7b1d3a9d7420adc41f69191fc72b3cae4b1a7ef7
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-07-11 15:05:36 +0900

    Add program to count number frequency in array
    Introduced 32_356_ex2.c, which counts and displays the frequency of numbers (0-9) in a predefined integer array using a counting array.

---
+++ c_program_bootcam2024/BASIC/32_cntArray.c/32_356_ex2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define SIZE 20
+
+int main()
+{
+    int sourceArr[SIZE] = {0,3,4,5,6,7,8,6,5,4,4,4,4,4,3,3,0,1,1,1}; 
+    int countArr[10] = {0}; 
+
+    int i ; 
+    for ( i = 0 ; i < SIZE; i++)
+    {
+        countArr[sourceArr[i]]++;
+    }
+    
+    // 결과 출력
+    printf("Number frequency:\n");
+    for (i = 0; i < 10; i++) {
+        if (countArr[i] > 0) {
+            printf("Number %d appears %d times\n", i, countArr[i]);
+        }
+    }
+
+	return 0;
+}
