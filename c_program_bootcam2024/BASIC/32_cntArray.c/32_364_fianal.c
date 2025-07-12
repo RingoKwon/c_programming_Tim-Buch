e506c75df85c85c8db2dc3167293d0a018528737
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-07-12 10:11:38 +0900

    Implement character counting for array elements
    Added logic to count occurrences of lowercase, uppercase, and space characters in sourceArr using countArr and idxArr. The code now maps each character to an index and increments the corresponding count.

--- c_program_bootcam2024/BASIC/32_cntArray.c/32_364_fianal.c
+++ c_program_bootcam2024/BASIC/32_cntArray.c/32_364_fianal.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define SIZE 10 
+#define SIZE 10
 /*
 [Problem]
 // Create an array to count occurrences of 'a'-'z', 'A'-'Z', and ' ' (space)
@@ -12,12 +12,30 @@
 */
 int main()
 {
-    char sourceArr[SIZE] = {'d', 'E', 'm', 'O', ' ', 't', 'e', 'x', 't','d'};
+    char sourceArr[SIZE] = {'d', 'E', 'm', 'O', ' ', 't', 'e', 'x', 't', 'd'};
+    char idxArr[SIZE];
     int countArr[26 + 26 + 1] = {0}; // 26 lowercase + 26 uppercase + 1 space
+    int i;
+    int lowerAdj = -97, upperAjd = -39, spaceAdj = +20; 
     // 'a' = 97, 'z'  = 122, 'A' = 65, 'Z' = 90, ' ' = 32
-    // - |a-z|: x-97  = 0~25 
+    // - |a-z|: x-97  = 0~25
     // - |A-Z|: x-39  = 26~51
-    // - ' '  : 32+20 = 52  
+    // - ' '  : 32+20 = 52
+
+    for (i = 0; i < SIZE; i++)
+    {
+        if (sourceArr[i] >= 'a' && sourceArr[i] <= 'z') {
+            idxArr[i] = sourceArr[i] + lowerAdj;
+            countArr[idxArr[i]]++;
+        } else if (sourceArr[i] >= 'A' && sourceArr[i] <= 'Z') {
+            idxArr[i] = sourceArr[i] + upperAjd;
+            countArr[idxArr[i]]++;
+        } else if (sourceArr[i] == ' ') {
+            idxArr[i] = sourceArr[i] + spaceAdj;
+            countArr[idxArr[i]]++;
+        }
+    }
+
 
 
     return 0;
