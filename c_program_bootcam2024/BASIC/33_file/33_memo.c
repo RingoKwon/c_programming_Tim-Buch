eb2b112660889c8c41873102480df5b2daa500d4
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-07-12 21:43:53 +0900

    Add file creation example in C
    Added 33_memo.c demonstrating how to create and open a file for writing in C. Also added an empty myFirstFile1.txt as the target file for the example.

---
+++ c_program_bootcam2024/BASIC/33_file/33_memo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    FILE* fp; // fp는 "file pointer"의 약자입니다.
+
+    fp = fopen("myFirstFile1.txt", "w");
+
+    if( fp != NULL )
+    {
+        printf("not null"); 
+        printf("%p", fp);
+        fclose(fp);
+    }
+
+    
+	return 0;
+}
