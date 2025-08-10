c083602bc267fffed0aa924d18ddd3b49fbb3d42
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-08-10 21:30:07 +0900

    303 my solution

--- c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_302_1toN.c
+++ c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_302_1toN.c
@@ -2,10 +2,19 @@
 
 void one2n(int nb)
 {
-    if (nb >= 1)
+    if (nb >= 2)
     {
         one2n(nb - 1);
         printf("%d ", nb);
+    }
+}
+
+void n2one(int nb)
+{
+    if (nb >= 1)
+    {
+        printf("%d ", nb);
+        n2one(nb - 1);
     }
 }
 
@@ -15,6 +24,7 @@ int main(void)
     
     printf("enterN: ");
     scanf("%d", &nb);
+    n2one(nb);
     one2n(nb);
     return 0;
 }
