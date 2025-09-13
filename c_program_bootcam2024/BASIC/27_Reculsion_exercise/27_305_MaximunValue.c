04dcef851470939ec17349ac809f0237e22e91e2
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-09-13 10:03:16 +0900

    scanf test

---
+++ c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_305_MaximunValue.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int maxValue()
+{
+    int num;
+    printf("Enter number: ");
+    scanf("%d", &num);
+    // base condition: num = -1
+    return 0;
+}
+int main()
+{
+    maxValue();
+    return 0;
+}
