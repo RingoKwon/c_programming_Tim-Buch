e79abd7055337327b168681d3baa431e4845106c
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-09-13 22:50:49 +0900

    digit sum start

---
+++ c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_308_DigitsSum_mysolution.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+// input = 123 -> 1 + 2 + 3 = 6(even) then 1 
+// input = 3123 -> 3 + 1 + 2 + 3 = 9(odd) then 0
+// 'digitSum', 'isEven'
+
+int digitSum(int num)
+{
+    return 0;
+}
+
+int isEven(int num)
+{
+    return 0;
+}
+
+int main(void)
+{
+    printf("test");
+}
