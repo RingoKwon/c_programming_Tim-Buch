7c055c362557f3047fe8ffa0a3b7ea933b84f5cc
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-09-13 10:14:29 +0900

    iteration test

--- c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_305_MaximunValue.c
+++ c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_305_MaximunValue.c
@@ -1,12 +1,18 @@
+// input num every time, base condition, compare num and decide bigger num.
 #include <stdio.h>
 
 int maxValue()
 {
     int num;
+    
     printf("Enter number: ");
     scanf("%d", &num);
     // base condition: num = -1
-    return 0;
+    if (num == -1)
+    {
+        return 0;
+    }
+    return maxValue();
 }
 int main()
 {
