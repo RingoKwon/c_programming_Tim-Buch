eff39bd4929459e0f0b0ddf4fbf67e2e33068fde
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-09-13 15:12:33 +0900

    wrong; re-start

--- c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_305_MaximumValue_mysolution.c
+++ c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_305_MaximumValue_mysolution.c
@@ -22,6 +22,33 @@ int maxValue(int maxnum)
 int main()
 {
     int fstnum;
+int maxValue(int maxnum)
+
+{
+    int num;
+    
+    printf("Enter number: ");
+    scanf("%d", &num);
+    if (num == -1)
+    {
+        printf("Maximum number is %d", maxnum);
+        return 0;
+    }
+    if (maxnum < num)
+    {
+        maxnum = num;
+    }
+    // base condition: num = -1
+    return maxValue(maxnum);
+}
+int main()
+{
+    int fstnum;
+    printf("Enter number: ");
+    scanf("%d", &fstnum);
+    maxValue(fstnum);
+    return 0;
+}
     printf("Enter number: ");
     scanf("%d", &fstnum);
     maxValue(fstnum);
--- c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_305_MaximumValue_solution.c
+++ c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_305_MaximumValue_solution.c
@@ -1,2 +1,24 @@
-// input num every time, base condition, compare num and decide bigger num.
 #include <stdio.h>
+
+int findMax(int num, int maxNum)
+{
+    int maxNum;
+    int inputNum;
+
+    if(num >= 1)
+    {
+        maxNum = findMax(num - 1);
+        printf("Enter number: ");
+        scanf("%d", &inputNum);
+        if(inputNum > maxNum)
+            return inputNum;
+        return maxNum;
+    }
+    printf("Maximum Number Is: %d", maxNum);
+    return 0;
+}
+
+int main(void)
+{
+    findMax(4);
+}
