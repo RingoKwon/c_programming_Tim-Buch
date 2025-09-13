ab6441864efc3a2082d0f1a513706e04be768601
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-09-13 10:26:00 +0900

    my solution

--- c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_305_MaximunValue.c
+++ c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_305_MaximunValue.c
@@ -1,21 +1,26 @@
 // input num every time, base condition, compare num and decide bigger num.
 #include <stdio.h>
 
-int maxValue()
+int maxValue(int maxnum)
 {
     int num;
     
     printf("Enter number: ");
     scanf("%d", &num);
-    // base condition: num = -1
     if (num == -1)
     {
+        printf("Maximun number is %d", maxnum);
         return 0;
     }
-    return maxValue();
+    if (maxnum < num)
+    {
+        maxnum = num;
+    }
+    // base condition: num = -1
+    return maxValue(maxnum);
 }
 int main()
 {
-    maxValue();
+    maxValue(0);
     return 0;
 }
