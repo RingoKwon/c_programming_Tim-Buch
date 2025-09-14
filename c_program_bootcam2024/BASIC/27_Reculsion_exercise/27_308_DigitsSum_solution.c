73b08031b418a868361ce1d62456f2893c1db37b
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-09-14 12:20:50 +0900

    done

---
+++ c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_308_DigitsSum_solution.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+// input = 123 -> 1 + 2 + 3 = 6(even) then 1
+// input = 3123 -> 3 + 1 + 2 + 3 = 9(odd) then 0
+// 'digitSum', 'isEven'
+//-----------------------------------------------
+// even + odd = odd; odd + odd = even; even + even = even;
+
+int isEven(int num)
+{
+    int result;
+
+    if(num < 10)
+    {
+        if(num % 2 == 0)
+            return 1;
+        return 0;
+    }
+    result = isEven(num / 10);
+    if ( (num % 10) % 2 == 0 )
+    {
+        if(result == 1)
+            return result = 1;
+        else
+            return result = 0;
+    }
+    else
+    {
+        if(result == 0)
+            return result = 1;
+        else
+            return result = 0;
+    }
+    
+    return -1;
+}
+
+int main(void)
+{
+    int output;
+
+    output = isEven(41);
+    printf("Output is %d", output);
+    return 0;
+}
