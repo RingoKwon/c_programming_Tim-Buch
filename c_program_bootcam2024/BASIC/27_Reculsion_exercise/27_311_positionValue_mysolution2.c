30b186bcd524c54f49d595a6525985a77662848f
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-09-17 00:25:04 +0900

    JT solution

--- c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_311_positionValue_mysolution2.c
+++ c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_311_positionValue_mysolution2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 // [Position Value]
 // if position is even -> digit is even. Otherwise odd.
 // ex num = 1234 -> return = 1 
@@ -13,25 +14,38 @@ int isSame(int position, int digit)
 int positionValue(int num, int position)
 {
     int digit;
+    int cumsum;
 
     digit = 0;
+    cumsum = 0;
 
     digit = num % 10;
     position++;
     if(num < 10)
     {
         printf("position: %d; digit: %d\n", position, digit);
-        return 1;
+        return cumsum;
     }
-    positionValue(num / 10, position);
-    printf("position: %d; digit: %d\n", position, digit);
+    cumsum = positionValue(num / 10, position);
+    printf("position: %d; digit: %d; cumsum: %d\n", position, digit, cumsum);
     if(isSame(position, digit) == 0)
-        return 0;
-    return 1;
+        return cumsum + 1 ;
+    return cumsum;
+}
+
+bool is_odd(int i) {
+    return i % 2 == 1;
+}
+
+bool recursive(int digit, bool pos) {
+    if (digit < 10) return !(is_odd(digit) ^ pos);
+    return !(is_odd(digit % 10) ^ pos) & recursive(digit / 10, !pos);
 }
 
 int main(void)
 {
-    positionValue(2233, -1);
+    bool ret1 = recursive( 1234, false );
+    printf("out: %d", ret1);
+    // positionValue(1234, -1);
     return 0;
 }
