914015f7be9f161edc79923d16a95e611fed7b54
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-09-17 21:06:19 +0900

    my solution

--- c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_311_positionValue_mysolution.c
+++ c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_311_positionValue_mysolution.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
+#include <stdbool.h>
 // [Position Value]
 // if position is even -> digit is even. Otherwise odd.
 // ex num = 1234 -> return = 1 
 // ex num = 1235 -> return = 0
+bool    isEven(int num)
+{
+    if(num % 2 == 0)
+        return 1;
+    return 0;
+}
+bool    isValid(int num, int position)
+{
+    int digit;
+
+    position++;
+    digit = num % 10;
+    if(num < 10)
+        return !(isEven(num) ^ isEven(position));
+    return !(isEven(digit) ^ isEven(position)) * isValid(num / 10, position);
+}
 
 int main(void)
 {
-    printf("Enter Number: \n");
+    int input;
+    printf("Enter Number: ");
+    scanf("%d", &input);
+    printf("output is %d\n", isValid(input, -1));
 }
