19f2623a59cee44a844c35307b7a7af0cbde120f
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-09-17 20:33:06 +0900

    ringo start ad JY solution

--- c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_311_positionValue_mysolution.c
+++ c_program_bootcam2024/BASIC/27_Reculsion_exercise/27_311_positionValue_mysolution.c
@@ -4,35 +4,7 @@
 // ex num = 1234 -> return = 1 
 // ex num = 1235 -> return = 0
 
-int conditionFlag(int position, int digit)
-{
-    if(position % 2 == digit % 2)
-        return 0; 
-    return 1;
-}
-
-int digitValue(int num, int position)
-{
-    int digit;
-    int sumerr;
-
-    digit = num % 10;
-    position++;
-
-    if(num < 10)
-    {
-        sumerr +=  conditionFlag(position, digit);
-        return sumerr;
-    }
-    digitValue(num / 10, position);
-    sumerr +=  conditionFlag(position, digit); 
-    return sumerr;
-}
-
 int main(void)
 {
-    int output;
     printf("Enter Number: \n");
-    output = digitValue(12345, -1);
-    printf("Output is %d", output);
 }
