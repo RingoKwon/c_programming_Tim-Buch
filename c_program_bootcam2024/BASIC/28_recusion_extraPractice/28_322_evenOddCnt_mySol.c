9b828aca80a5661036a8e29b11a2695952072a6b
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-09-20 17:39:50 +0900

    my solution 2

--- c_program_bootcam2024/BASIC/28_recusion_extraPractice/28_322_evenOddCnt_mySol.c
+++ c_program_bootcam2024/BASIC/28_recusion_extraPractice/28_322_evenOddCnt_mySol.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
-
-int cntEven(int num)
+// input: int num, int digit 
+// output: 
+//  - 1: if the count of 'digit' occurences in num is even.
+//  - 0; otherwise (if the numver of 'digit' occurenves is odd).
+//  for example
+//  num = 124, digit = 2 --> 0 
+//  num = 12342, difit = 2 --> 1
+int cntDigit(int num, int digit)
 {
     if (num < 10)
-        return num % 2 == 0;
-    return (num % 10 % 2 == 0) + cntEven(num / 10);
+        return num == digit;
+    return (num % 10 == digit) + cntDigit(num / 10, digit);
 }
 
-int validation(int num, int cnt)
+int validation(int num, int digit)
 {
-    int evenCntNum;
+    int digitCntNum;
 
-    evenCntNum = cntEven(num);
-    if (evenCntNum == cnt)
+    digitCntNum = cntDigit(num, digit); 
+    printf("digitCntNum: %d\n", digitCntNum);
+    if (digitCntNum % 2 == 0)
         return 1;
     return 0;
 }
@@ -20,7 +27,8 @@ int validation(int num, int cnt)
 int main(void)
 {
     int output;
-    output =  validation(1213458, 3);
-    printf("%d", output);
+    output =  validation(1223334444, 4);
+    printf("Answer is ");
+    printf("%d\n", output);
     return 0;
 }
