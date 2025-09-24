c24a4f2f3f450cbabc1ad410e63b9a3a513ac7d3
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-09-24 21:13:30 +0900

    fgets study

---
+++ c_program_bootcam2024/BASIC/33_file/33_375_fgets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    FILE *fp;
+
+    char str[10];
+    int cnt;
+
+    fp = fopen("myFirst3.txt", "r");
+    if (fp != NULL)
+    {
+        
+        while(fgets(str, 10, fp ))
+            printf("sting #%d: %s\n", cnt++, str);
+    }
+    
+    fclose(fp);
+    return 0;
+}
