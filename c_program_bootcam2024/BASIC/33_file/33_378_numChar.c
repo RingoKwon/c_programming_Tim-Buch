b6b23500c989e95ab552dd7a2d79278b9404365f
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-09-25 00:20:38 +0900

    my solution

--- c_program_bootcam2024/BASIC/33_file/33_378_numChar.c
+++ c_program_bootcam2024/BASIC/33_file/33_378_numChar.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-// using fgetc count char
+/*
+1. using fgetc count char
+2. how many line this file has
+*/
 
 int main(void)
 {
     FILE *fp;
-    int c;  // ✅ int 타입 사용! not char
+    int c;  //   int 타입 사용! not char
     int cnt;
+    int lCnt;
 
     fp = fopen("exerciseFile.txt", "r");
     
     if (fp != NULL)
     {
         cnt  = 0;
+        lCnt = 1;
         while ((c = fgetc(fp)) != EOF)
-            printf("# %d: %c\n",++cnt, c);
+        {
+            printf("# %d: %c",++cnt, c);
+            printf("lCnt = %d\n", c == '\n');
+            if (c == '\n')
+                ++lCnt;
+        }
         fclose(fp);
     }
+    printf("lines: %d", lCnt);
     return 0;
 }
