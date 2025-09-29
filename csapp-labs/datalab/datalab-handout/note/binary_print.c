259972219bb0a7de292d502a082c2bd4fae30825
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-09-30 00:37:02 +0900

    comp: binary

--- csapp-labs/datalab/datalab-handout/note/binary_print.c
+++ csapp-labs/datalab/datalab-handout/note/binary_print.c
@@ -30,44 +30,10 @@ int binary_prt_logical(int n)
     printf("\n");
     return 0;
 }
-int binary_digits(int n)
-{
-    int i;
-    int p;
-
-    i = 0;
-    while (pow(2, i) <= n)
-    {
-        // printf("(%d)", (int)pow(2,i));
-        i++;
-    }
-    if (n == 0)
-        i = 0;
-    else 
-        i -= 1;
-    printf("n: %d\n", n);
-    printf("d: %d\n", i);
-    printf("\n");
-    return 0;
-}
-int binary_prt_arithmetical(int n)
-{
-    if (n < 2)
-        printf("%d", n % 2);
-    printf("\n");
-    return 0;
-}
 
 int main(void)
 {
-    printf("sizeof(int) = %zu bytes\n", sizeof(int)); // %zu: size_t
-    printf("INT_MIN = %d\n", INT_MIN);
-    printf("INT_MAX = %d\n", INT_MAX);
-    printf("--------------------------------\n");
-    printf("%d\n", -2 >> 30 & 1);
-    binary_prt_logical(INT_MIN);
-    binary_prt_arithmetical(INT_MIN);
-    binary_digits(0);
+    binary_prt_logical(-100);
     return 0;
 
 }
