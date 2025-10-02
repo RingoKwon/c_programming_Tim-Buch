9850259a7e00ac2e1c33831c4852e2295666a90f
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-10-03 07:53:50 +0900

    correct

--- c_program_bootcam2024/BASIC/34_interview/swap.c
+++ c_program_bootcam2024/BASIC/34_interview/swap.c
@@ -6,11 +6,12 @@ int main(void)
     int a;
     int b;
 
-    a = 4;
-    b = 2;
+    a = 7;
+    b = -3;
 
     a = a - b;  
     b = a + b; 
+    a = - a + b;
     printf("a: %d, b: %d", a, b);
     return 0;
 }
