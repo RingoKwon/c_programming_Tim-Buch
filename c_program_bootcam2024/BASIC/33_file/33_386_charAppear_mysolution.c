3e5c3c859bc8e5005497007a069949a5698c17e9
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-10-03 13:52:46 +0900

    solution drop

--- c_program_bootcam2024/BASIC/33_file/33_386_charAppear_mysolution.c
+++ c_program_bootcam2024/BASIC/33_file/33_386_charAppear_mysolution.c
@@ -6,17 +6,22 @@ int main(void)
 
     char    str[100];
     char    c;
-    // int     i;
+    int     i;
+    int     j;
 
     scanf("%s", str);
-    printf("%s", str);
     
     fp = fopen("exFile.txt", "r");
     if (fp != NULL)
     {
-        // i = 0;
+        i = 0;
         while ((c = fgetc(fp)) != EOF)
         {
+            j = 0;
+            while (str[j] != '\0')
+            {
+                j++; 
+            }// af cnt j become cnt(str) 4char => 1 
             printf("%c", c);
         }
         fclose(fp);
