8a2166554b26cebe8a1fe920c4ea70669f741ba8
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-10-05 16:38:19 +0900

    alphabet d /Users/ringokwon/Project/cProject/c_program_bootcam2024/BASIC/33_file/ && gcc 33_389_lowerFreqAppear_mySol.c -o 33_389_lowerFreqAppear_mySol && /Users/ringokwon/Project/cProject/c_program_bootcam2024/BASIC/33_file/33_389_lowerFreqAppear_mySol cd /Users/ringokwon/Project/cProject/c_program_bootcam2024/BASIC/33_file/ && gcc 33_389_lowerFreqAppear_mySol.c -o 33_389_lowerFreqAppear_mySol && /Users/ringokwon/Project/cProject/c_program_bootcam2024/BASIC/33_file/33_389_lowerFreqAppear_mySol cd /Users/ringokwon/Project/cProject/c_program_bootcam2024/BASIC/33_file/ && gcc 33_389_lowerFreqAppear_mySol.c -o 33_389_lowerFreqAppear_mySol && /Users/ringokwon/Project/cProject/c_program_bootcam2024/BASIC/33_file/33_389_lowerFreqAppear_mySol cd /Users/ringokwon/Project/cProject/c_program_bootcam2024/BASIC/33_file/ && gcc 33_389_lowerFreqAppear_mySol.c -o 33_389_lowerFreqAppear_mySol && /Users/ringokwon/Project/cProject/c_program_bootcam2024/BASIC/33_file/33_389_lowerFreqAppear_mySol

--- c_program_bootcam2024/BASIC/33_file/33_389_lowerFreqAppear_mySol.c
+++ c_program_bootcam2024/BASIC/33_file/33_389_lowerFreqAppear_mySol.c
@@ -1,20 +1,29 @@
-#include <stdio.h>
 /*
 Write a prgram that reads a filename frem the user.
 The program should create a "frequencyAppearances" array 
 for lowercase letters ('a' - 'z') and 
 print the number of times each lowercase letter appears in file.
  */
+#include <stdio.h>
+#define SIZE 26
 
 int main(void)
 {
     char    c;
     char    filename[100] = {0};
-    int     arr;
+    int     arr[SIZE] = {9,9};
+    int     i;
+
     printf("Enter Designated Chatacter: ");
     scanf("%c", &c);
     printf("Enter Filename: ");
     scanf("%s", filename);
 
+    i = 0;
+    while (i < SIZE )
+    {
+        printf("%c", 'a' + i);
+        i++;
+    }
     return 0;
 }
