71f0a0336a1ab0e6b0cf1af58f5cc2a9df08b65c
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-10-07 19:45:13 +0900

    not working

---
+++ c_program_bootcam2024/Advanced/S5_ArrNPtr/5_44_voidReverseRecursive_Solutio.c
@@ -0,0 +1,45 @@
+// void reverse recursive 
+#include <stdio.h>
+#define SIZE 5
+
+void swap(char *arr1, char *arr2)
+{
+    char* tmp;
+    *tmp = *arr1;
+    *arr1 = *arr2;
+    *arr2 = *tmp;
+}
+
+void    prt(char *str, int size)
+{
+    int i;
+
+    i = 0;
+    while (i < size)
+    {
+        printf("%c", str[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+void    iterativeReverse(char* str, int size)
+{
+    int i;
+    i = 0;
+    while (i < size / 2)
+    {
+        // printf("i: %d, size - i - 1: %d\n", i, size - i - 1);
+        swap(&str[i], &str[size - i - 1]);
+        i++;
+    }
+}
+
+
+
+int main(void)
+{
+    char str[SIZE] = {'a', 'b', 'c', 'd', 'e'};
+    iterativeReverse(str, SIZE);
+    return (0);
+}
