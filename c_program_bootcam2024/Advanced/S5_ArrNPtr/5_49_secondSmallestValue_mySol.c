67580a88825c9ae8e1222d21ba67db2f40ecbc0e
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-10-08 15:14:59 +0900

    my solution

---
+++ c_program_bootcam2024/Advanced/S5_ArrNPtr/5_49_secondSmallestValue_mySol.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#define SIZE 5
+
+int smallest(int* arr, int size)
+{
+    int i;
+    int min;
+
+    i = 1;
+    min = arr[0];
+    while (i < size)
+    {
+        if (min > arr[i])
+            min = arr[i];
+        i++;
+    }
+    return min;
+}
+
+int findSecondSmall(int* arr, int size)
+{
+    int i;
+    int min;
+    int min2nd;
+
+    min = smallest(arr, SIZE);
+    i = 0;
+    min2nd = 10000;
+    while (i < SIZE)
+    {
+        if ( arr[i] > min && arr[i] < min2nd)
+        {
+            min2nd = arr[i];
+        }
+        i++;
+    }
+    return min2nd;
+}
+
+int main(void)
+{
+    int arr[SIZE] = {1,2,3,4,5};
+    findSecondSmall(arr, SIZE);
+    printf("min %d\n", smallest(arr, SIZE));
+    printf("2nd min %d\n", findSecondSmall(arr, SIZE));
+    return (0);
+}
