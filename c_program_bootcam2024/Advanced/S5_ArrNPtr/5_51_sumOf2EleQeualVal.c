0e2887be1e8d37c8e8f748b09fd47ca7f2953020
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-10-08 19:39:47 +0900

    Vlad solution

--- c_program_bootcam2024/Advanced/S5_ArrNPtr/5_51_sumOf2EleQeualVal.c
+++ c_program_bootcam2024/Advanced/S5_ArrNPtr/5_51_sumOf2EleQeualVal.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #define SIZE 5
 
-int checkConMy(int* arr, int val, int* idx1, int* idx2)
+int checkConMy(int* arr,int size ,int val, int* idx1, int* idx2)
 {
     int i;
     int j;
 
     i = 0;
-    while (i < SIZE)
+    while (i < size)
     {
         j = i + 1;
-        while (j < SIZE )
+        while (j < size)
         {
             // printf("i: %d, j: %d\n", arr[i], arr[j]);
             if (val == arr[i] + arr[j])
@@ -23,6 +23,35 @@ int checkConMy(int* arr, int val, int* idx1, int* idx2)
         }
         i++;
     }
+    *idx1 = 0;
+    *idx2 = 0;
+    return (0);
+}
+
+int checkConVladOpt(int* arr, int size, int val, int *idx1, int *idx2)
+{
+    int i;
+    int j;
+    int sumVal;
+
+    i = 0;
+    j = size - 1;
+    while (i != j)
+    {
+        sumVal = arr[i] + arr[j];
+        if (sumVal == val)
+        {
+            *idx1 = i;
+            *idx2 = j;
+            return (1);
+        }
+        if (sumVal > val)
+            j--;
+        else
+            i++;
+        }
+    *idx2 = 0;
+    *idx1 = 0;
     return (0);
 }
 
@@ -34,7 +63,9 @@ int main(void)
     int idx1;
     int idx2;
 
-    result = checkConMy(arr, val, &idx1, &idx2);
-    printf("Result: %d,  %d + %d", result, arr[idx1],  arr[idx2]);
+    result = checkConMy(arr, SIZE, val, &idx1, &idx2);
+    printf("My Result: %d,  %d + %d\n", result, arr[idx1],  arr[idx2]);
+    result = checkConVladOpt(arr, SIZE, val, &idx1, &idx2);
+    printf("OPT Result: %d,  %d + %d\n", result, arr[idx1],  arr[idx2]);
     return (0);
 }
