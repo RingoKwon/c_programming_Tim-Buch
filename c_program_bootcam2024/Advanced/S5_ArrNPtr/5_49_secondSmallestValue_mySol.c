6cf84777f9a0e9f5864f9b950d3fc8739514140b
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-10-08 15:56:10 +0900

    solution

--- c_program_bootcam2024/Advanced/S5_ArrNPtr/5_49_secondSmallestValue_mySol.c
+++ c_program_bootcam2024/Advanced/S5_ArrNPtr/5_49_secondSmallestValue_mySol.c
@@ -54,10 +54,36 @@ int findSecondSmall(int* arr, int size)
     return min2nd;
 }
 
+int small2ndVlad(int* arr, int size)
+{
+    int i;
+    int min1;
+    int min2;
+
+    i = 0;
+    min1 = arr[0];
+    min2 = arr[0];
+    while (i < SIZE)
+    {
+        if (arr[i] < min1)
+        {
+            min2 = min1;
+            min1 = arr[i];
+        }
+        else if (arr[i] < min2 || min1 == min2) // important
+        {
+            min2 = arr[i];
+        }
+        i++;
+    }
+    return min2;
+}
+
 int main(void)
 {
     int arr[SIZE] = {1,2,3,4,5};
     printf("min %d\n", smallest(arr, SIZE));
     printf("2nd min %d\n", findSecondSmall(arr, SIZE));
+    printf("2nd min(Vlad) %d\n", small2ndVlad(arr, SIZE));
     return (0);
 }
