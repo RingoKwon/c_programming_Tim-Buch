493c3011b9a2309a1abe232fe83b05870d08d7c3
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-10-08 22:18:50 +0900

    my solution

--- c_program_bootcam2024/Advanced/S5_ArrNPtr/5_56_palindrome.c
+++ c_program_bootcam2024/Advanced/S5_ArrNPtr/5_56_palindrome.c
@@ -5,24 +5,22 @@
 
 int palindromeMy(int* arr, int size)
 {
-    if (size == 0)
+    if (size <= 1)
         return (1);
     if (arr[0] != arr[size - 1])
     {
-        return (0);
+        return 0 * palindromeMy(arr + 1, size - 2);
     }
-    else 
-        return (1);
-    return palindromeMy(arr + 1, size - 2);
+    return 1 * palindromeMy(arr + 1, size - 2);
 }
 
 int main(void)
 {
     int arr1[SIZE1] = {1,2,3,2,1};
-    int arr2[SIZE2] = {1,2,3,3,2,1};
+    int arr2[SIZE2] = {1,1,3,3,1,1};
     int result;
 
-    result = palindromeMy(arr1, SIZE1);
+    result = palindromeMy(arr2, SIZE2);
     printf("%d", result);
     return (0);
 }
