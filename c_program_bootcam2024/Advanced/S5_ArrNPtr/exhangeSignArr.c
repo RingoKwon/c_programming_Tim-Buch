0f28c023e2568d44a9024704b8daf24453a4df96
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-10-09 12:06:04 +0900

    other solution

--- c_program_bootcam2024/Advanced/S5_ArrNPtr/exhangeSignArr.c
+++ c_program_bootcam2024/Advanced/S5_ArrNPtr/exhangeSignArr.c
@@ -28,10 +28,21 @@ int arrCheckMy2(int* arr, int size)
         return arrCheckMy(arr + 1, size - 1);
 }
 
+int arrCheckMy3(int* arr, int size)
+{
+    if (size == 1)
+        return (1);
+    if (!signCheck(arr[0], arr[1]))
+        return (0);
+    else
+        return arrCheckMy(&arr[1], size - 1);
+}
+
 int main(void)
 {
-    int arr[SIZE] = {-1,2,3,4,-5};
+    int arr[SIZE] = {-1,2,-3,4,-5};
     printf("%d\n", arrCheckMy(arr, SIZE));
     printf("%d\n", arrCheckMy2(arr, SIZE));
+    printf("%d\n", arrCheckMy3(arr, SIZE));
     return (0);
 }
