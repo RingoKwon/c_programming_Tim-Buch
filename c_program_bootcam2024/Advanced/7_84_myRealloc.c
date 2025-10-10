c150bed54a7443d4b953f1f809caf04b1d13a7d6
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-10-10 14:04:01 +0900

    error void* universal pointer

--- c_program_bootcam2024/Advanced/7_84_myRealloc.c
+++ c_program_bootcam2024/Advanced/7_84_myRealloc.c
@@ -4,15 +4,23 @@
 
 void* myRealloc(void* srcblock, unsigned oldsize, unsigned newsize)
 {
-    void* tmp;
+    char* tmp;
+    char i;
 
     tmp = (void*)malloc(newsize);
     if (tmp == NULL)
     {
-        printf("Error");
+        printf("Realloc Failed");
         return (0);
     }
-    printf("Sucess");
+
+    i = 0;
+    while (i < oldsize) 
+    {
+        tmp[i] = (char)srcblock[i];
+        i++;
+    }
+    printf("The realloc Sucessed\n");
     return tmp;
 }
 
@@ -23,6 +31,13 @@ int main(void)
     
     newsize = SIZE + 2;
     arr = (int*)malloc(sizeof(int) * SIZE);
+    if (arr == NULL)
+    {
+        printf("Malloc Failed\n");
+        return (0);
+    }
+
+
     myRealloc(arr, SIZE * sizeof(int), newsize);
     return (0);
 }
