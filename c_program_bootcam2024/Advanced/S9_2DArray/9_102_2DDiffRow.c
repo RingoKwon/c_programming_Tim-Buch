748f5c6cec66cd5423b67d37ccc13fc7cb3314f9
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-10-14 20:54:20 +0900

    my solition

--- c_program_bootcam2024/Advanced/S9_2DArray/9_102_2DDiffRow.c
+++ c_program_bootcam2024/Advanced/S9_2DArray/9_102_2DDiffRow.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#define SIZE 5
 int main(void)
 {
+    int* arr[SIZE];
+    size_t i;
+    size_t r_size;
+
+    i = 0;
+    while(i < SIZE)
+    {
+
+        if(i == 0)
+            r_size = 1;
+        else if ( i == 2 || i == 3)
+            r_size = 2;
+        else
+            r_size = 3;
+        arr[i] = (int*)malloc(sizeof(int) * r_size);
+        i++;
+    }
     return (0);
 }
