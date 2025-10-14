a3c1267d0557d0470bc816d33e06f5f3ad395325
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-10-14 15:58:48 +0900

    size adjustment

--- c_program_bootcam2024/Advanced/S8_DMA_Advanced_Exercise/8_95_universalInsert_VladSolution.c
+++ c_program_bootcam2024/Advanced/S8_DMA_Advanced_Exercise/8_95_universalInsert_VladSolution.c
@@ -14,26 +14,44 @@ void    prtArr(double* arr, size_t size)
     }
     printf("\n");  // Added newline
 }
+// all size based on bytes
+void*   universalInsertVlad(void* arr, size_t* size,
+                            void* ptr, size_t idx, size_t ptrSize)
+{
+    char*   newArr;
+
+    newArr = (char*)malloc(*size + ptrSize);
+    memcpy(newArr, arr, idx);
+    memcpy(newArr, arr, idx);
+
+    *size += ptrSize;
+    return (newArr);
+}
 
 int main(void)
 {
     double arr[] = {1.1, 2.2, 3.3};  // Changed from double* to double[]
     size_t size;
-    double *arr2;
-    
+    double* arr2;
+    double* newArr;
+    double src;
+
+    src = 9.99;
     arr2 = (double*)malloc(sizeof(double) * 3);
     if (!arr2)
     {
         fprintf(stderr, "Memory allocation Failed\n");
-        return 1;  // Return error code (non-zero)
+        // return 1;  // Return error code (non-zero)
     }
-
-    
-    size = 3;
-    prtArr(arr, size);
+    size = sizeof(double) * 3;
+    prtArr(arr, 3);
     // memcpy;
     memcpy(arr2 + 1, arr, sizeof(double) * 1);
     printf("memcpy\n");
-    prtArr(arr2, size);
+    prtArr(arr2, 3);
+    free(arr2);
+    printf("universalInsert\n");
+    newArr = universalInsertVlad(arr, &size, &src, sizeof(double) * 1, sizeof(double));
+    prtArr(newArr, size / sizeof(double));
     return (0);
 }
