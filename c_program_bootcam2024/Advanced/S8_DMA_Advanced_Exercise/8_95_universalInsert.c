112f7b33a8ee0ab1dc8e4b127b3c88729e41f752
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-10-14 12:55:24 +0900

    mysolution

--- c_program_bootcam2024/Advanced/S8_DMA_Advanced_Exercise/8_95_universalInsert.c
+++ c_program_bootcam2024/Advanced/S8_DMA_Advanced_Exercise/8_95_universalInsert.c
@@ -7,6 +7,7 @@ void*   universalInsert(void* arr, size_t *arrSize,
     void* newArr = (void*)malloc((typeSize *  (*arrSize + 1)));
     size_t i;
     size_t j;
+    size_t k;
 
     if (!newArr)
     {
@@ -15,25 +16,54 @@ void*   universalInsert(void* arr, size_t *arrSize,
     }
     i = 0;
     j = 0;
+    k = 0;
     while (i < (typeSize *  (*arrSize + 1)))
     {
-        ((char*)newArr)[i] = ((char*)arr)[j];
-        printf(" %zu", i);
-        i++;
-        j++;
+        if (i >= (idx * typeSize) && i < ((idx + 1) * typeSize))
+        {
+            ((char*)newArr)[i] = ((char*)ptr)[k]; 
+            i++;
+            k++;
+            printf("i: %zu, k: %zu\n", i, k);
+        }
+        else
+        {
+            ((char*)newArr)[i] = ((char*)arr)[j];
+            i++;
+            j++;
+            printf("i: %zu, j: %zu\n", i, j);
+        }
     }
+    *arrSize = *arrSize + 1;
 
     return (newArr);
 }
+
+
 int main(void)
 {
     float arr[] = {1, 2, 3};
+    float* newArr;
     size_t size;
     float ptr;
+    size_t i;
 
     size = 3;
     ptr = 1.1;
-    universalInsert(arr, &size, &ptr, 1, sizeof(float) );
+
+    newArr = (float*)malloc(sizeof(float) * (size + 1));
+    if (!newArr)
+    {
+        printf("Memory Allocation Failed");
+        free(newArr);
+    }
+    newArr = universalInsert(arr, &size, &ptr, 4, sizeof(float) );
+    i = 0;
+    while(i < size)
+    {
+        printf("%f ", newArr[i]);
+        i++;
+    }
     return (0);
 
 }
