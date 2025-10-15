afd014597108f679ea207343ccf2545752489092
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-10-15 13:51:39 +0900

    final 2dAlloc started and freeMatrix needed

--- c_program_bootcam2024/Advanced/S9_2DArray/9_00_dynamic2dArrFinal.c
+++ c_program_bootcam2024/Advanced/S9_2DArray/9_00_dynamic2dArrFinal.c
@@ -2,6 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 
+int** create2dArr(int rows, int cols)
+{
+    int** arr;
+    size_t  i;
+
+    arr = (int**)calloc(rows, sizeof(int*));
+    if (!arr)
+    {
+        printf("Memory Allocation Failed");
+        return (NULL);
+    }
+    i = 0;
+    while (i < cols)
+    {
+        *arr = (int*)calloc(cols, sizeof(int));
+        i++;
+    }
+    return (arr);
+}
+
+
+
 int main(void)
 {
     return (0);
