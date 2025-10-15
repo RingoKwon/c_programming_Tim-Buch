630be3556ad8198d479217eb10a44492938343ca
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-10-15 22:29:12 +0900

    test successfull

--- c_program_bootcam2024/Advanced/S9_2DArray/9_107_swap2dArr.c
+++ c_program_bootcam2024/Advanced/S9_2DArray/9_107_swap2dArr.c
@@ -7,13 +7,14 @@ void freeMatrix(int** arr, size_t rows)
     while (rows > 0)
     {
         free(arr[rows - 1]);
+        printf("rows: %zu\n", rows - 1);// test
         rows--;
     }
     free(arr);
     printf("Memory Freed\n");
 }
 
-int** creat2dArr(size_t rows, size_t cols)
+int** create2dArr(size_t rows, size_t cols)
 {
     int** arr;
     int i;
@@ -27,9 +28,11 @@ int** creat2dArr(size_t rows, size_t cols)
     while (i < rows)
     {
         *arr = (int*)calloc(rows, sizeof(int));
+        if(i == 2)// test   
+            *arr = NULL;
         if (!*arr)
         {
-            freeMatrix(arr, rows);
+            freeMatrix(arr, i);
             return (NULL);
         }
         i++;
@@ -39,5 +42,12 @@ int** creat2dArr(size_t rows, size_t cols)
 
 int main(void)
 {
+    int** arr;
+    size_t rows;
+    size_t cols;
+
+    rows = 4;
+    cols = 4;
+    arr = create2dArr(rows, cols);
     return (0);
 }
