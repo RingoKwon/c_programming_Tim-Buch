608f0dc0acfa2d1ecbaf86bf1e9d2f94d3d93eca
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-10-15 15:39:27 +0900

    error static 2d arr

--- c_program_bootcam2024/Advanced/S9_2DArray/swap2d.c
+++ c_program_bootcam2024/Advanced/S9_2DArray/swap2d.c
@@ -5,7 +5,33 @@
 a. swap rows in: r1, r2; O(1);
 b. swap cols in: c1, c2; O(n);
 */
+
+void prt2dArr(int** arr, size_t cols, size_t rows)
+{
+    size_t i;
+    size_t j;
+
+    i = 0;
+    while (i < rows)
+    {
+        j = 0;
+        while (j > cols)
+        {
+            printf("%d ", arr[i][j]);
+            j++;
+        }
+        i++;
+    }
+}
+
 int main(void)
 {
+    int arr[3][2] = {{1,2}, {10, 20}, {30, 40}};
+    size_t rows;
+    size_t cols;
+
+    rows = 3;
+    cols = 2;
+    prt2dArr(arr, rows, cols);
     return (0);
 }
