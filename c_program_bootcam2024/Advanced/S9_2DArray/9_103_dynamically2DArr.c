48845f8162ba6d75b608e6204099399f38f805e9
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-10-15 11:18:53 +0900

    my free solution

--- c_program_bootcam2024/Advanced/S9_2DArray/9_103_dynamically2DArr.c
+++ c_program_bootcam2024/Advanced/S9_2DArray/9_103_dynamically2DArr.c
@@ -2,6 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+void    freeMatrix(int** arr, size_t size)
+{
+    int i;
+
+    while (size > 0)
+    {
+        free(arr[size]);
+        size--;
+    }
+}
+
 int main(void)
 {
     int** arr;
@@ -24,7 +35,10 @@ int main(void)
         scanf("%zu", &cols);
         arr[i] = (int*)calloc(cols, sizeof(int));
         if (!arr[i])
+        {
+            freeMatrix(arr, i);
             return (1);
+        }
         i++;
     }
     
