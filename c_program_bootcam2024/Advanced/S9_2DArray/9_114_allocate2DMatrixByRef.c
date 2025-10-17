b7d5e7e951120a07e69232a19b876143317d5cb7
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-10-18 00:42:37 +0900

    before review

--- c_program_bootcam2024/Advanced/S9_2DArray/9_114_allocate2DMatrixByRef.c
+++ c_program_bootcam2024/Advanced/S9_2DArray/9_114_allocate2DMatrixByRef.c
@@ -2,16 +2,48 @@
 #include <stdlib.h>
 #include <string.h>
 
-void   allocate2DMatrixByRef(int*** arr)
+void    free2DArr(int** arr, size_t rows)
 {
+    while (rows > 0)
+    {
+        free(arr[rows - 1]);
+        rows--;
+    }
+    free(arr);
+    printf("Memory Freed");
+}
+
+void   allocate2DMatrixByRef(int*** arr, size_t rows, size_t cols)
+{
+    size_t i;
+
     *arr = (int**)calloc(3, sizeof(int*));
+    if (!*arr)
+    {
+        printf("Memory Allocation Failed");
+        return ;
+    }
+    i = 0;
+    while (i < rows)
+    {
+        (*arr)[i] = (int*)calloc(cols, sizeof(int));
+        if (!(*arr)[i])
+            free2DArr(*arr, i);
+        i++;
+    }
 }
 
 int main(void)
 {
     int** arr;
+    size_t rows;
+    size_t cols;
 
-    allocate2DMatrixByRef(&arr);
-    free(arr);
+    printf("Enter rows: ");
+    scanf("%zu", &rows);
+    printf("Enter cols: ");
+    scanf("%zu", &cols);
+    allocate2DMatrixByRef(&arr, rows, cols);
+    free2DArr(arr, rows);
     return (0);
 }
