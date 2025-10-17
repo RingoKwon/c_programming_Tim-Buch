780df817fc8ad92a83595a52d317d5ed9eb8ad87
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-10-18 00:11:54 +0900

    my solution

--- c_program_bootcam2024/Advanced/S9_2DArray/9_114_allocate2DMatrixByRef.c
+++ c_program_bootcam2024/Advanced/S9_2DArray/9_114_allocate2DMatrixByRef.c
@@ -2,9 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-
+void   allocate2DMatrixByRef(int*** arr)
+{
+    *arr = (int**)calloc(3, sizeof(int*));
+}
 
 int main(void)
 {
-    retutn (0);
+    int** arr;
+
+    allocate2DMatrixByRef(&arr);
+    free(arr);
+    return (0);
 }
