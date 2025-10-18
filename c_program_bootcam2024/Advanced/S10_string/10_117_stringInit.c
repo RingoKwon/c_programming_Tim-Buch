0ecea76c606826d3d5c60e49408dc2c403e4483a
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-10-18 09:30:03 +0900

    save

---
+++ c_program_bootcam2024/Advanced/S10_string/10_117_stringInit.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    char    charArr[] = {'H', 'e', 'l', 'l', 'o', '\n'};
+
+    char    str[] = "Hello";
+
+    char*   str2 = "Hello";
+
+    char*   tempPtr;
+    tempPtr = charArr;
+    tempPtr[0] = 'R';
+    printf("#1\n\n");
+    printf("charArr[]: %s", charArr);
+    printf("tempPtr: %s", tempPtr);
+    printf("----------\n#2 *str\n\n");
+    printf("str[]: %s", str);
+    // str = "ggg";// not assignable;
+    str[0] = 'g';
+    printf("-> %s", str);
+    printf("\n----------\n#3 str[]\n\n");
+    str2 = "yellow";
+    printf("str[] = \"yelloe\": %s", str2);
+    // str2[0] = 'H'; bus error;
+    
+    return (0);
+}
