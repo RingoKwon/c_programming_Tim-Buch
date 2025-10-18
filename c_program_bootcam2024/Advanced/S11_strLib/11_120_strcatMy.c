19edaaca1cba607b1311ec8b9c40ed81591e631f
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-10-18 10:58:08 +0900

    my solution not working

---
+++ c_program_bootcam2024/Advanced/S11_strLib/11_120_strcatMy.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int strlenMy(char* str)
+{
+    size_t i;
+
+    i = 0;
+    while (str[i])
+    {
+        i++;
+    }
+    return (i);
+}
+
+void strcatMy(char** dest, char* src)
+{
+    size_t destLen;
+    size_t srcLen;
+    size_t  newLen;
+    size_t i;
+    char*   newStr;
+
+    destLen = strlenMy(*dest);
+    srcLen = strlenMy(src);
+    newLen = destLen + srcLen;
+    newStr = (char*)malloc(sizeof(char) * (newLen + 1)) ;
+    i = 0;
+    while (i < destLen)
+    {
+        newStr[i] = (*dest)[i];
+        i++;
+    }
+    while (i < srcLen)
+    {
+        newStr[i] = src[i];
+        i++;
+    }
+    newStr[i] = '\0';
+    *dest = newStr;
+}
+
+int main(void)
+{
+    char*   src = "Ringo ";
+    char*   dest = "Hello";
+    printf("%d\n", strlenMy(src));
+    strcatMy(&dest, src);
+    printf("%s", dest);
+    return (0);
+}
