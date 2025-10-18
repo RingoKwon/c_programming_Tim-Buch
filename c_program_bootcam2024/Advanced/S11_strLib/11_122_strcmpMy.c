aeeaa932a356bf00cad44ad27a1d3cebc3b881e1
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-10-18 17:14:35 +0900

    Vlad solution

--- c_program_bootcam2024/Advanced/S11_strLib/11_122_strcmpMy.c
+++ c_program_bootcam2024/Advanced/S11_strLib/11_122_strcmpMy.c
@@ -11,12 +11,12 @@ int strcmpMy(char* str1, char* str2)
     {
         if (str1[i] > str2[i])
         {
-            res = -1;
+            res = 1;
             break;
         }
         else if (str1[i] < str2[i])
         {
-            res = 1;
+            res = -1;
             break;
         }
         res = 0;
@@ -25,13 +25,36 @@ int strcmpMy(char* str1, char* str2)
     return (res);
 }
 
+int strcmpFlag(char* str1, char* str2)
+{
+    int flag;
+    size_t i;
+
+    i = 0;
+    flag = 0;
+    while (flag == 0)
+    {
+        if (str1[i] > str2[i])
+            flag = 1;
+        else if (str1[i] < str2[i])
+            flag = -1;
+        if (str1[i] == '\0')
+            break;
+        i++;
+    }
+    return (flag);
+}
+
 int main(void)
 {
     char str1[10] = "rIngo";
     char str2[10] = "ringo";
     int res;
+    int res2;
 
     res = strcmpMy(str1, str2);
-    printf("%d", res);
+    res2 = strcmpFlag(str1, str2);
+    printf("%d\n", res);
+    printf("%d\n", res2);
     return (0);
 }
