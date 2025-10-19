07fd357230562de39bc97ae61dd7727e80e0d503
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-10-19 18:39:04 +0900

    my solution done

--- c_program_bootcam2024/Advanced/S13_struct/13_135-1_structDynamicAllocArr.c
+++ c_program_bootcam2024/Advanced/S13_struct/13_135-1_structDynamicAllocArr.c
@@ -40,14 +40,39 @@ Employee*   alloc1(size_t size)
     return (eply);
 }
 
+void   alloc2(Employee** eply, size_t size)
+{
+    size_t  i;
+
+
+    *eply = (Employee*)malloc(sizeof(Employee) * size);
+    i = 0;
+    while (i < size)
+    {
+        printf("Enther %zuth name: ", i + 1);
+        scanf("%s", (*eply)[i].name);
+        printf("Enter %zuth age: ", i + 1);
+        scanf("%f", &((*eply)[i].age));
+        printf("Enter %zuth id: ", i + 1);
+        scanf("%d", &((*eply)[i].id));
+        i++;
+    }
+}
+
 int main(void)
 {
     size_t size;
 
     printf("Enter size: ");
     scanf("%zu", &size);
+    printf("[Return approach]\n");
     Employee* emp1;
     emp1 = alloc1(size);
     prtEply(emp1, size);
+
+    printf("[Reference approach]\n");
+    Employee* emp2;
+    alloc2(&emp2, size);
+    prtEply(emp2, size);
     return (0);
 }
